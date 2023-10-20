4ebe260ae2c51ef18ad461227254d9da2b1433db
Yoo Ha Young <116700717+Yoo-Ha-young@users.noreply.github.com>
2023-10-20 11:05:49 +0900

    Delete node/database.js

--- node/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2/promise");
-
-exports.pool = mysql.createPool({
-    host: "www.dailyplan.shop",
-    user: "hayoung93",
-    port: "3306",
-    password: "qwer1234",
-    database: "DailyPlanDB",
-});
