9315a1818cb775709e6bdc0cfe21dd9345ca3a28
Yoo-Ha-Young <bluekitten74@naver.com>
2023-10-20 11:54:53 +0900

    clear git cache

--- node/secret.js
+++
@@ -1,9 +0,0 @@
-exports.jwtSecret = "jwt-secret-8520"
-
-exports.databaseSecret = {
-    host: "www.dailyplan.shop",
-    user: "hayoung93",
-    port: "3306",
-    password: "qwer1234",
-    database: "DailyPlanDB",
-};
